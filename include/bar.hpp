8d5b61a9fddc0db9614eeb7e2a952852d36b132f
Alexis <alexisr245@gmail.com>
2018-08-11 10:25:21 +0200

    refactor(bar): default width is 0

--- include/bar.hpp
+++ include/bar.hpp
@@ -31,14 +31,6 @@ namespace waybar {
       const char *name);
     static void _handleDescription(void *data,
       struct zxdg_output_v1 *zxdg_output_v1, const char *description);
-    static void _handleGeometry(void *data, struct wl_output *wl_output,
-      int32_t x, int32_t y, int32_t physical_width, int32_t physical_height,
-      int32_t subpixel, const char *make, const char *model, int32_t transform);
-    static void _handleMode(void *data, struct wl_output *wl_output,
-      uint32_t f, int32_t w, int32_t h, int32_t refresh);
-    static void _handleDone(void *data, struct wl_output *);
-    static void _handleScale(void *data, struct wl_output *wl_output,
-      int32_t factor);
     static void _layerSurfaceHandleConfigure(void *data,
       struct zwlr_layer_surface_v1 *surface, uint32_t serial, uint32_t width,
       uint32_t height);
@@ -47,7 +39,7 @@ namespace waybar {
     auto _setupConfig() -> void;
     auto _setupWidgets() -> void;
     auto _setupCss() -> void;
-    uint32_t _width = 10;
+    uint32_t _width = 0;
     uint32_t _height = 30;
     Json::Value _config;
     Glib::RefPtr<Gtk::StyleContext> _styleContext;
