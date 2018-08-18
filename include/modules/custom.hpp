b794ca63d1f8cd8eefe918a903e3afe0e218b61d
Alexis <alexisr245@gmail.com>
2018-08-18 17:27:40 +0200

    feat(custom): exec-if

--- include/modules/custom.hpp
+++ include/modules/custom.hpp
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <fmt/format.h>
+#include <iostream>
+#include <sys/wait.h>
 #include "util/chrono.hpp"
 #include "ALabel.hpp"
 
@@ -8,10 +10,10 @@ namespace waybar::modules {
 
 class Custom : public ALabel {
   public:
-    Custom(const std::string&, Json::Value);
+    Custom(std::string, Json::Value);
     auto update() -> void;
   private:
-    const std::string& name_;
+    std::string name_;
     waybar::util::SleeperThread thread_;
 };
 
