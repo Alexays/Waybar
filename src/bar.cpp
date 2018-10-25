45847847b975594dd3cb519b3c83c9e31685dc1a
Alexis <alexisr245@gmail.com>
2018-10-25 10:43:37 +0200

    style: disallow resize

--- src/bar.cpp
+++ src/bar.cpp
@@ -21,6 +21,7 @@ waybar::Bar::Bar(const Client& client,
 	zxdg_output_v1_add_listener(xdg_output_, &xdgOutputListener, this);
   window.set_title("waybar");
   window.set_decorated(false);
+  window.set_resizable(false);
   setupConfig();
   setupCss();
   setupWidgets();
