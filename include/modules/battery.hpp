d8b620163212c670acde4cb56c4b9297b9652e4f
Lucas L. Treffenstädt <lucas@treffenstaedt.de>
2018-11-02 22:15:54 +0100

    ...and fix the function signature in the header

--- include/modules/battery.hpp
+++ include/modules/battery.hpp
@@ -23,7 +23,7 @@ class Battery : public ALabel {
   
     void worker();
     std::tuple<uint16_t, std::string> getInfos();
-    std::string getState(uint16_t, bool);
+    std::string getState(uint16_t);
 
     util::SleeperThread thread_;
     util::SleeperThread threadTimer_;
