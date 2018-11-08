1fe66a2c9aee462315c433d15cdc0ea9cf8daf1a
Robinhuett <5955614+Robinhuett@users.noreply.github.com>
2018-11-08 21:09:56 +0100

    Use /proc/meminfo for Memory module

--- src/modules/memory.cpp
+++ src/modules/memory.cpp
@@ -13,13 +13,30 @@ waybar::modules::Memory::Memory(const Json::Value& config)
 
 auto waybar::modules::Memory::update() -> void
 {
-  struct sysinfo info = {};
-  if (sysinfo(&info) == 0) {
-    auto total = info.totalram * info.mem_unit;
-    auto freeram = info.freeram * info.mem_unit;
-    int used_ram_percentage = 100 * (total - freeram) / total;
-    label_.set_text(fmt::format(format_, used_ram_percentage));
-    auto used_ram_gigabytes = (total - freeram) / std::pow(1024, 3);
-    label_.set_tooltip_text(fmt::format("{:.{}f}Gb used", used_ram_gigabytes, 1));
+  parseMeminfo();
+  int used_ram_percentage = 100 * (memtotal_ - memfree_) / memtotal_;
+  label_.set_text(fmt::format(format_, used_ram_percentage));
+  auto used_ram_gigabytes = (memtotal_ - memfree_) / std::pow(1024, 2);
+  label_.set_tooltip_text(fmt::format("{:.{}f}Gb used", used_ram_gigabytes, 1));
+}
+
+void waybar::modules::Memory::parseMeminfo()
+{
+  int memtotal, memfree, memavail, membuffer, memcache;
+  FILE* info = fopen("/proc/meminfo","r");
+  if(fscanf (info, "MemTotal: %d kB MemFree: %d kB Buffers: %d kB Cached: %d kB",&memtotal, &memfree, &membuffer, &memcache) < 4) { // Old meminfo format 
+    fclose(info);
+    info = fopen("/proc/meminfo","r"); 
+    if(fscanf(info, "MemTotal: %d kB MemFree: %d kB MemAvailable: %d kB Buffers: %d kB Cached: %d kB",&memtotal, &memfree, &memavail, &membuffer, &memcache) < 5) { // Current meminfo format
+      memtotal_ = -1;
+      memfree_ = -1;
+    } else {
+      memtotal_ = memtotal;
+      memfree_ = memavail;
+    }
+  } else {
+    memtotal_ = memtotal;
+    memfree_ = memfree + (membuffer + memcache);
   }
+  fclose(info);
 }
