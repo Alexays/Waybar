4877d9f005ca20b12eeb3c3ffe38a077f28a7ca4
Alexis <alexisr245@gmail.com>
2018-11-15 14:44:43 +0100

    feat(cpu): add both usage and load

--- include/modules/cpu.hpp
+++ include/modules/cpu.hpp
@@ -1,9 +1,11 @@
 #pragma once
 
 #include <fmt/format.h>
+#include <sys/sysinfo.h>
 #include <fstream>
 #include <vector>
 #include <numeric>
+#include <iostream>
 #include "util/chrono.hpp"
 #include "ALabel.hpp"
 
@@ -15,8 +17,11 @@ class Cpu : public ALabel {
     auto update() -> void;
   private:
     static inline const std::string data_dir_ = "/proc/stat";
-    std::vector< std::tuple<size_t, size_t> > parseCpuinfo();
-    std::vector< std::tuple<size_t, size_t> > prevTimes_;
+    uint16_t getCpuLoad();
+    std::tuple<uint16_t, std::string> getCpuUsage();
+    std::vector<std::tuple<size_t, size_t>> parseCpuinfo();
+
+    std::vector<std::tuple<size_t, size_t>> prev_times_;
     waybar::util::SleeperThread thread_;
 };
 
