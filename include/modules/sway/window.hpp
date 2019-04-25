1f008f67c30889f8bd34613290c039b469a93277
Alex <alexisr245@gmail.com>
2019-04-25 16:47:51 +0200

    feat(Bar): add class depend of window in the workspace

--- include/modules/sway/window.hpp
+++ include/modules/sway/window.hpp
@@ -17,17 +17,18 @@ class Window : public ALabel {
   auto update() -> void;
 
  private:
-  void                         onEvent(const struct Ipc::ipc_response&);
-  void                         onCmd(const struct Ipc::ipc_response&);
-  void                         worker();
-  std::tuple<int, std::string> getFocusedNode(const Json::Value& nodes);
-  void                         getFocusedWindow();
+  void                                                   onEvent(const struct Ipc::ipc_response&);
+  void                                                   onCmd(const struct Ipc::ipc_response&);
+  void                                                   worker();
+  std::tuple<std::size_t, int, std::string, std::string> getFocusedNode(const Json::Value& nodes);
+  void                                                   getTree();
 
   const Bar&                  bar_;
   waybar::util::SleeperThread thread_;
   Ipc                         ipc_;
   std::string                 window_;
   int                         windowId_;
+  std::string                 app_id_;
 };
 
 }  // namespace waybar::modules::sway
