c6c3a1e41dac1fbf4ac22fe1807bd4d2aae7a205
Alex <alexisr245@gmail.com>
2019-05-20 14:39:49 +0200

    fix: remove workaround

--- src/modules/clock.cpp
+++ src/modules/clock.cpp
@@ -14,11 +14,7 @@ waybar::modules::Clock::Clock(const std::string& id, const Json::Value& config)
     auto sub_m =
         std::chrono::duration_cast<std::chrono::seconds>(time_s.time_since_epoch()).count() %
         interval_.count();
-    if (sub_m > 0) {
-      thread_.sleep_until(timeout - std::chrono::seconds(sub_m - 1));
-    } else {
-      thread_.sleep_until(timeout - std::chrono::seconds(sub_m));
-    }
+    thread_.sleep_until(timeout - std::chrono::seconds(sub_m));
   };
 }
 
