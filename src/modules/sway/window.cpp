1561818c31a61d60e97e28f2b00730682e6e5b00
Alex <alexisr245@gmail.com>
2019-06-11 17:57:17 +0200

    fix(window): avoid hexpand

--- src/modules/sway/window.cpp
+++ src/modules/sway/window.cpp
@@ -6,7 +6,6 @@ namespace waybar::modules::sway {
 Window::Window(const std::string& id, const Bar& bar, const Json::Value& config)
     : ALabel(config, "window", id, "{}"), bar_(bar), windowId_(-1) {
   if (label_.get_max_width_chars() == -1) {
-    label_.set_hexpand(true);
     label_.set_ellipsize(Pango::EllipsizeMode::ELLIPSIZE_END);
   }
   ipc_.subscribe(R"(["window","workspace"])");
