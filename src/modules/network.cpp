Alexays/Waybar

commit c1427ff8072aa9573a09941fbaa1c45af5005d89

network: Handle carrier information
IFLA_CARRIER allows to know when a cable is plugged to the Ethernet
card or when the WiFi is connected. If there's no carrier, the
interface will be considered disconnected.

src/modules/network.cpp
@@ -292,6 +292,7 @@ const std::string waybar::modules::Network::getNetworkState() const {
 #endif
     return "disconnected";
   }
+  if (!carrier_) return "disconnected";
   if (ipaddr_.empty()) return "linked";
   if (essid_.empty()) return "ethernet";
   return "wifi";
@@ -402,6 +403,7 @@ void waybar::modules::Network::clearIface() {
   essid_.clear();
   ipaddr_.clear();
   netmask_.clear();
+  carrier_ = false;
   cidr_ = 0;
   signal_strength_dbm_ = 0;
   signal_strength_ = 0;
@@ -423,6 +425,7 @@ int waybar::modules::Network::handleEvents(struct nl_msg *msg, void *data) {
     struct rtattr *ifla = IFLA_RTA(ifi);
     const char *ifname = NULL;
     size_t ifname_len = 0;
+    bool carrier = false;
 
     if (net->ifid_ != -1 && ifi->ifi_index != net->ifid_) {
       return NL_OK;
@@ -434,6 +437,10 @@ int waybar::modules::Network::handleEvents(struct nl_msg *msg, void *data) {
         ifname = static_cast<const char *>(RTA_DATA(ifla));
         ifname_len = RTA_PAYLOAD(ifla) - 1; // minus \0
         break;
+      case IFLA_CARRIER: {
+        carrier = *(char*)RTA_DATA(ifla) == 1;
+        break;
+      }
       }
     }
 
@@ -443,6 +450,7 @@ int waybar::modules::Network::handleEvents(struct nl_msg *msg, void *data) {
         std::string new_ifname (ifname, ifname_len);
         net->ifname_ = new_ifname;
       }
+      net->carrier_ = carrier;
     } else if (!is_del_event && net->ifid_ == -1) {
       // Checking if it's an interface we care about.
       std::string new_ifname (ifname, ifname_len);
@@ -451,6 +459,7 @@ int waybar::modules::Network::handleEvents(struct nl_msg *msg, void *data) {
 
         net->ifname_ = new_ifname;
         net->ifid_ = ifi->ifi_index;
+        net->carrier_ = carrier;
         net->thread_timer_.wake_up();
         /* An address for this new interface should be received via an
          * RTM_NEWADDR event either because we ask for a dump of both links

include/modules/network.hpp
@@ -64,6 +64,7 @@ class Network : public ALabel {
 
   std::string state_;
   std::string essid_;
+  bool        carrier_;
   std::string ifname_;
   std::string ipaddr_;
   std::string netmask_;

commit 28dfb0ba41f2d0f97b2f84de77e878c6c24fc29d

network: Fix use of carrier information
Some RTM_NEWLINK messages may not have the IFLA_CARRIER information.
This is the case when a WiFi interface report scan result are
available. `carrier` is used regardless of if it is present in the
message or not. This would result in the interface appearing
"disconnected" in waybar when it isn't.

This patch now check that `carrier` is available before using it.

The same thing could potentially happen to `ifname` so check if it's
set before recording it.

Fixes: c1427ff (network: Handle carrier information)
Fixes #388

src/modules/network.cpp
@@ -425,7 +425,7 @@ int waybar::modules::Network::handleEvents(struct nl_msg *msg, void *data) {
     struct rtattr *ifla = IFLA_RTA(ifi);
     const char *ifname = NULL;
     size_t ifname_len = 0;
-    bool carrier = false;
+    std::optional<bool> carrier;
 
     if (net->ifid_ != -1 && ifi->ifi_index != net->ifid_) {
       return NL_OK;
@@ -446,11 +446,13 @@ int waybar::modules::Network::handleEvents(struct nl_msg *msg, void *data) {
 
     if (!is_del_event && ifi->ifi_index == net->ifid_) {
       // Update inferface information
-      if (net->ifname_.empty()) {
+      if (net->ifname_.empty() && ifname != NULL) {
         std::string new_ifname (ifname, ifname_len);
         net->ifname_ = new_ifname;
       }
-      net->carrier_ = carrier;
+      if (carrier.has_value()) {
+        net->carrier_ = carrier.value();
+      }
     } else if (!is_del_event && net->ifid_ == -1) {
       // Checking if it's an interface we care about.
       std::string new_ifname (ifname, ifname_len);
@@ -459,7 +461,9 @@ int waybar::modules::Network::handleEvents(struct nl_msg *msg, void *data) {
 
         net->ifname_ = new_ifname;
         net->ifid_ = ifi->ifi_index;
-        net->carrier_ = carrier;
+        if (carrier.has_value()) {
+          net->carrier_ = carrier.value();
+        }
         net->thread_timer_.wake_up();
         /* An address for this new interface should be received via an
          * RTM_NEWADDR event either because we ask for a dump of both links