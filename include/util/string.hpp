061cb76fc48f493b187928f841083981e9733a28
Alex Maystrenko <alexeytech@gmail.com>
2022-02-05 20:47:29 +0100

    make functions in header file inline
    added header guard
    now string utils can be used in any part of the project

--- include/util/string.hpp
+++ include/util/string.hpp
@@ -1,15 +1,17 @@
+#pragma once
+
 #include <string>
 
 const std::string WHITESPACE = " \n\r\t\f\v";
 
-std::string ltrim(const std::string s) {
+inline std::string ltrim(const std::string s) {
   size_t begin = s.find_first_not_of(WHITESPACE);
   return (begin == std::string::npos) ? "" : s.substr(begin);
 }
 
-std::string rtrim(const std::string s) {
+inline std::string rtrim(const std::string s) {
   size_t end = s.find_last_not_of(WHITESPACE);
   return (end == std::string::npos) ? "" : s.substr(0, end + 1);
 }
 
-std::string trim(const std::string& s) { return rtrim(ltrim(s)); }
+inline std::string trim(const std::string& s) { return rtrim(ltrim(s)); }
