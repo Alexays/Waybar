2697d432a4c271f1cd6b701799842688c0e4f85e
Alex Maystrenko <alexeytech@gmail.com>
2022-02-05 20:54:06 +0100

    ltrim and rtrim take argument by const-ref

--- include/util/string.hpp
+++ include/util/string.hpp
@@ -4,12 +4,12 @@
 
 const std::string WHITESPACE = " \n\r\t\f\v";
 
-inline std::string ltrim(const std::string s) {
+inline std::string ltrim(const std::string& s) {
   size_t begin = s.find_first_not_of(WHITESPACE);
   return (begin == std::string::npos) ? "" : s.substr(begin);
 }
 
-inline std::string rtrim(const std::string s) {
+inline std::string rtrim(const std::string& s) {
   size_t end = s.find_last_not_of(WHITESPACE);
   return (end == std::string::npos) ? "" : s.substr(0, end + 1);
 }
