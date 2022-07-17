04d66de866c58ff22546ecd647380597690ef7e7
Marvin Dostal <maffinmaffinmaffinmaffin@gmail.com>
2022-07-17 22:20:24 +0200

    sni: remove unnecesary parameter

--- src/modules/sni/item.cpp
+++ src/modules/sni/item.cpp
@@ -345,7 +345,7 @@ Glib::RefPtr<Gdk::Pixbuf> Item::getIconPixbuf() {
   }
 
   if (icon_name.empty()) {
-    spdlog::error("Item '{}': No icon name or pixmap given.", id, icon_name);
+    spdlog::error("Item '{}': No icon name or pixmap given.", id);
   } else {
     spdlog::error("Item '{}': Could not find an icon named '{}' and no pixmap given.", id, icon_name);
   }
