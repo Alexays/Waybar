e25a7c9719dbc7e81f6397b07bd32092fbd1df1e
Viktar Lukashonak <myxabeer@gmail.com>
2023-02-28 15:32:28 +0300

    ISSUE#1977. AModule implements module actions call
    Signed-off-by: Viktar Lukashonak <myxabeer@gmail.com>

--- include/IModule.hpp
+++ include/IModule.hpp
@@ -9,6 +9,7 @@ class IModule {
   virtual ~IModule() = default;
   virtual auto update() -> void = 0;
   virtual operator Gtk::Widget&() = 0;
+  virtual auto doAction(const std::string& name) -> void = 0;
 };
 
 }  // namespace waybar
