b8630968b262726b01c00a34a65907ca86eed784
Austin Horstman <khaneliman12@gmail.com>
2023-09-09 13:23:17 -0500

    refactor: move capitalize string helper

--- include/util/enum.hpp
+++ include/util/enum.hpp
@@ -6,32 +6,24 @@
 #include <stdexcept>
 #include <string>
 
+#include "util/string.hpp"
+
 namespace waybar::util {
 
 template <typename EnumType>
 struct EnumParser {
   EnumParser() {}
 
-  // Helper function to capitalize a string
-  std::string capitalizeString(const std::string& str) {
-    std::string result = str;
-    std::transform(result.begin(), result.end(), result.begin(),
-                   [](unsigned char c) { return std::toupper(c); });
-    return result;
-  }
-
   EnumType parseStringToEnum(const std::string& str,
                              const std::map<std::string, EnumType>& enumMap) {
     // Convert the input string to uppercase
-    std::string uppercaseStr = capitalizeString(str);
+    std::string uppercaseStr = capitalize(str);
 
     // Capitalize the map keys before searching
     std::map<std::string, EnumType> capitalizedEnumMap;
-    std::transform(enumMap.begin(), enumMap.end(),
-                   std::inserter(capitalizedEnumMap, capitalizedEnumMap.end()),
-                   [this](const auto& pair) {
-                     return std::make_pair(capitalizeString(pair.first), pair.second);
-                   });
+    std::transform(
+        enumMap.begin(), enumMap.end(), std::inserter(capitalizedEnumMap, capitalizedEnumMap.end()),
+        [this](const auto& pair) { return std::make_pair(capitalize(pair.first), pair.second); });
 
     // Return enum match of string
     auto it = enumMap.find(uppercaseStr);
