8ce64ea784ca3345735cef7de9464583abd965dc
Austin Horstman <khaneliman12@gmail.com>
2023-09-09 09:38:03 -0500

    refactor: make parsing sort-by more lenient

--- include/util/enum.hpp
+++ include/util/enum.hpp
@@ -12,10 +12,16 @@ struct EnumParser {
   enum SORT_METHOD { ID, NAME, NUMBER, DEFAULT };
 
   SORT_METHOD sortStringToEnum(const std::string& str) {
+    // Convert the input string to uppercase (make it lenient on config input)
+    std::string uppercaseStr;
+    for (char c : str) {
+        uppercaseStr += std::toupper(c);
+    }
+
     static const std::map<std::string, SORT_METHOD> enumMap = {
         {"ID", ID}, {"NAME", NAME}, {"NUMBER", NUMBER}, {"DEFAULT", DEFAULT}};
 
-    auto it = enumMap.find(str);
+    auto it = enumMap.find(uppercaseStr);
     if (it != enumMap.end()) {
       return it->second;
     } else {
