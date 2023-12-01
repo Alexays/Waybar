2e2cb67cf14e709966fd3a6001e2af97614d8877
chayleaf <chayleaf-git@pavluk.org>
2023-12-02 01:10:19 +0700

    flake: update & cleanup

--- flake.nix
+++ flake.nix
@@ -3,26 +3,19 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    devshell.url = "github:numtide/devshell";
-    flake-utils.url = "github:numtide/flake-utils";
     flake-compat = {
       url = "github:edolstra/flake-compat";
       flake = false;
     };
   };
 
-  outputs = { self, flake-utils, devshell, nixpkgs, flake-compat }:
+  outputs = { self, nixpkgs, ... }:
     let
       inherit (nixpkgs) lib;
-      genSystems = lib.genAttrs [
+      genSystems = func: lib.genAttrs [
         "x86_64-linux"
         "aarch64-linux"
-      ];
-
-      pkgsFor = genSystems (system:
-        import nixpkgs {
-          inherit system;
-        });
+      ] (system: func (import nixpkgs { inherit system; }));
 
       mkDate = longDate: (lib.concatStringsSep "-" [
         (builtins.substring 0 4 longDate)
@@ -33,58 +26,36 @@
     {
       overlays.default = final: prev: {
         waybar = final.callPackage ./nix/default.nix {
-          version = prev.waybar.version + "+date=" + (mkDate (self.lastModifiedDate or "19700101")) + "_" + (self.shortRev or "dirty");
+          # take the first "version: '...'" from meson.build
+          version =
+            (builtins.head (builtins.split "'"
+              (builtins.elemAt
+                (builtins.split " version: '" (builtins.readFile ./meson.build))
+                2)))
+            + "+date=" + (mkDate (self.lastModifiedDate or "19700101")) + "_" + (self.shortRev or "dirty");
         };
       };
-      packages = genSystems
-        (system:
-          (self.overlays.default pkgsFor.${system} pkgsFor.${system})
-          // {
-            default = self.packages.${system}.waybar;
-          });
+      packages = genSystems (pkgs:
+        let packages = self.overlays.default pkgs pkgs;
+        in packages // {
+          default = packages.waybar;
+        });
     } //
-    flake-utils.lib.eachDefaultSystem (system: {
-      devShell =
-        let pkgs = import nixpkgs {
-          inherit system;
+    genSystems (pkgs: {
+      devShells.default =
+        pkgs.mkShell {
+          name = "waybar-shell";
 
-          overlays = [ devshell.overlays.default ];
-        };
-        in
-        pkgs.devshell.mkShell {
-          imports = [ "${pkgs.devshell.extraModulesDir}/language/c.nix" ];
+          # most of these aren't actually used in the waybar derivation, this is just in case
+          # they will ever start being used
+          inherit (pkgs.waybar) buildInputs depsBuildBuild depsBuildBuildPropagated depsBuildTarget
+            depsBuildTargetPropagated depsHostHost depsHostHostPropagated depsTargetTarget
+            depsTargetTargetPropagated propagatedBuildInputs propagatedNativeBuildInputs strictDeps;
 
-          devshell.packages = with pkgs; [
+          nativeBuildInputs = pkgs.waybar.nativeBuildInputs ++ (with pkgs; [
             clang-tools
             gdb
-            # from nativeBuildInputs
-            gnumake
-            meson
-            ninja
-            pkg-config
-            scdoc
-          ] ++ (map lib.getDev [
-            # from buildInputs
-            wayland wlroots gtkmm3 libsigcxx jsoncpp spdlog gtk-layer-shell howard-hinnant-date libxkbcommon
-            # optional dependencies
-            gobject-introspection glib playerctl python3.pkgs.pygobject3
-            libevdev libinput libjack2 libmpdclient playerctl libnl
-            libpulseaudio sndio sway libdbusmenu-gtk3 udev upower wireplumber
-
-            # from propagated build inputs?
-            at-spi2-atk atkmm cairo cairomm catch2 fmt_8 fontconfig
-            gdk-pixbuf glibmm gtk3 harfbuzz pango pangomm wayland-protocols
           ]);
-
-          env = with pkgs; [
-            { name = "CPLUS_INCLUDE_PATH"; prefix = "$DEVSHELL_DIR/include"; }
-            { name = "PKG_CONFIG_PATH"; prefix = "$DEVSHELL_DIR/lib/pkgconfig"; }
-            { name = "PKG_CONFIG_PATH"; prefix = "$DEVSHELL_DIR/share/pkgconfig"; }
-            { name = "PATH"; prefix = "${wayland.bin}/bin"; }
-            { name = "LIBRARY_PATH"; prefix = "${lib.getLib sndio}/lib"; }
-            { name = "LIBRARY_PATH"; prefix = "${lib.getLib zlib}/lib"; }
-            { name = "LIBRARY_PATH"; prefix = "${lib.getLib howard-hinnant-date}/lib"; }
-          ];
         };
     });
 }
