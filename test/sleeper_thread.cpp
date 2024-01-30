83a8159b564d3e2096ff0ff65b1c5f8ef448ef28
Tamino Bauknecht <dev@tb6.eu>
2024-01-30 17:22:07 +0100

    test: Improve SleeperThread tests

--- test/sleeper_thread.cpp
+++ test/sleeper_thread.cpp
@@ -1,5 +1,8 @@
 #include "util/sleeper_thread.hpp"
 
+#include <condition_variable>
+#include <mutex>
+
 #if __has_include(<catch2/catch_test_macros.hpp>)
 #include <catch2/catch_test_macros.hpp>
 #else
@@ -14,10 +17,7 @@ TEST_CASE("Threaded execution", "[SleeperThread]") {
     functionWasCalled = true;
     callingThreadId = std::this_thread::get_id();
   };
-  {
-    waybar::util::SleeperThread testThread(mockFunction);
-    std::this_thread::sleep_for(std::chrono::milliseconds(20));
-  }
+  { waybar::util::SleeperThread testThread(mockFunction); }
   CHECK(callingThreadId != std::this_thread::get_id());
   REQUIRE(functionWasCalled);
 }
@@ -25,37 +25,52 @@ TEST_CASE("Threaded execution", "[SleeperThread]") {
 TEST_CASE("Sleep", "[SleeperThread]") {
   waybar::util::CancellationGuard cancelGuard;
   std::atomic<int> counter = 0;
-  waybar::util::SleeperThread testThread([&counter, &testThread]() {
+  std::mutex waitMutex;
+  std::unique_lock waitLock{waitMutex};
+  std::condition_variable waitVar;
+  waybar::util::SleeperThread testThread([&counter, &testThread, &waitVar]() {
+    ++counter;
+    waitVar.notify_all();
     testThread.sleep();
     ++counter;
+    waitVar.notify_all();
   });
-  std::this_thread::sleep_for(std::chrono::milliseconds(5));
-  CHECK(counter == 0);
-  testThread.wake_up();
-  std::this_thread::sleep_for(std::chrono::milliseconds(5));
-  REQUIRE(counter == 1);
+  waitVar.wait_for(waitLock, std::chrono::seconds{5}, [&counter]() { return counter > 0; });
+  CHECK(counter >= 1);
+  waitVar.wait_for(waitLock, std::chrono::seconds{5}, [&counter, &testThread]() {
+    testThread.wake_up();
+    return counter > 1;
+  });
+  REQUIRE(counter >= 2);
 }
 
 TEST_CASE("Sleep_for", "[SleeperThread]") {
   waybar::util::CancellationGuard cancelGuard;
   std::atomic<int> counter = 0;
-  waybar::util::SleeperThread testThread([&counter, &testThread]() {
-    testThread.sleep_for(std::chrono::milliseconds(10));
-    ++counter;
-  });
-  CHECK(counter == 0);
-  std::this_thread::sleep_for(std::chrono::milliseconds(20));
-  REQUIRE(counter > 0);
+  const auto start = std::chrono::steady_clock::now();
+  {
+    waybar::util::SleeperThread testThread([&counter, &testThread, start]() {
+      testThread.sleep_for(std::chrono::milliseconds(10));
+      counter = std::chrono::duration_cast<std::chrono::milliseconds>(
+                    std::chrono::steady_clock::now() - start)
+                    .count();
+    });
+    std::this_thread::sleep_for(std::chrono::milliseconds(30));
+  }
+  REQUIRE(counter > 10);
 }
 
 TEST_CASE("Sleep_until", "[SleeperThread]") {
   waybar::util::CancellationGuard cancelGuard;
   std::atomic<int> counter = 0;
-  waybar::util::SleeperThread testThread([&counter, &testThread]() {
+  const auto start = std::chrono::steady_clock::now();
+  waybar::util::SleeperThread testThread([&counter, &testThread, start]() {
     testThread.sleep_until(std::chrono::system_clock::now() + std::chrono::milliseconds(10));
-    ++counter;
+    counter = std::chrono::duration_cast<std::chrono::milliseconds>(
+                  std::chrono::steady_clock::now() - start)
+                  .count();
   });
   CHECK(counter == 0);
-  std::this_thread::sleep_for(std::chrono::milliseconds(20));
-  REQUIRE(counter > 0);
+  std::this_thread::sleep_for(std::chrono::milliseconds(30));
+  REQUIRE(counter > 10);
 }
