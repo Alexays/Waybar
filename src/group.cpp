0572e02d7e04fbac20c5f881ef778ef5e6b8dd67
Alex <alexisr245@gmail.com>
2024-05-06 10:51:30 +0200

    fix: lint

--- src/group.cpp
+++ src/group.cpp
@@ -89,8 +89,6 @@ bool Group::handleMouseEnter(GdkEventCrossing* const& e) {
   return false;
 }
 
-
-
 bool Group::handleMouseLeave(GdkEventCrossing* const& e) {
   box.unset_state_flags(Gtk::StateFlags::STATE_FLAG_PRELIGHT);
   revealer.set_reveal_child(false);
