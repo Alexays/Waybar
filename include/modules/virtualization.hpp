c5e8d135d91674db62fb57434b55372ca627cb5a
leongross <leon.gross@9elements.com>
2025-04-16 17:45:37 +0200

    virt: improve VMInfo abstractions
    Signed-off-by: leongross <leon.gross@9elements.com>

--- include/modules/virtualization.hpp
+++ include/modules/virtualization.hpp
@@ -13,6 +13,31 @@
 
 namespace waybar::modules {
 
+const std::string LIBVIRT_STATE_MAP[] = {"NOSTATE",  "RUNNING", "BLOCKED", "PAUSED",
+                                         "SHUTDOWN", "SHUTOFF", "CRASHED", "PMSUSPENDED"};
+class VMInfo {
+ public:
+  VMInfo(const std::string name, const virDomainInfo info) : name_(name), info_(info) {}
+
+  std::string getName() const { return name_; }
+  virDomainInfo getInfo() const { return info_; }
+
+  bool operator==(const VMInfo &other) const {
+    return name_ == other.name_ && info_.state == other.info_.state &&
+           info_.maxMem == other.info_.maxMem && info_.memory == other.info_.memory &&
+           info_.cpuTime == other.info_.cpuTime;
+  }
+
+  std::string string() const {
+    return fmt::format("Name: {}, State: {}, MaxMem: {}, Memory: {}, CPUTime: {}", name_,
+                       LIBVIRT_STATE_MAP[info_.state], info_.maxMem, info_.memory, info_.cpuTime);
+  }
+
+ private:
+  std::string name_;
+  virDomainInfo info_;
+};
+
 class Virtualization : public ALabel {
  public:
   Virtualization(const std::string &, const Json::Value &);
@@ -41,12 +66,18 @@ class Virtualization : public ALabel {
   // If the domain does not exist, throw an error.
   void getDomainInfo(const std::string domain, virDomainInfoPtr info);
 
+  // Get the domain info for all domains
+  std::vector<VMInfo> getAllDomains();
+
   // For a list of domain infos, calculate the total cpu and memory and the max
   // they can consume.
-  virDomainInfo getTotalDomainStats(std::vector<virDomainInfo> &domainInfos);
+  virDomainInfo getTotalDomainStats(std::vector<VMInfo> &VMInfos);
 
   // Convert a factor of time or size for scaling
   float convertFactor(std::string divisor);
+
+  // Round a float to a given precision
+  float roundToPrecision(float num, int precision);
 };
 
 }  // namespace waybar::modules
