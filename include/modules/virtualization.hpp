bbeafac6e73b1ddbdd1f4490253db758a3b51e8e
leongross <leon.gross@9elements.com>
2025-04-16 17:45:37 +0200

    virt: improve VMInfo abstractions
    Signed-off-by: leongross <leon.gross@9elements.com>

--- include/modules/virtualization.hpp
+++ include/modules/virtualization.hpp
@@ -13,6 +13,31 @@
 
 namespace waybar::modules {
 
+const std::string LIBVIRT_STATE_MAP[] = {"NOSTATE",  "RUNNING", "BLOCKED", "PAUSED",
+                                         "SHUTDOWN", "SHUTOFF", "CRASHED", "PMSUSPENDED"};
+class VMInfo {
+ public:
+  VMInfo(const std::string name, const virDomainInfo info) : name_(name), info_(info) {}
+
+  std::string getName() const { return name_; }
+  virDomainInfo getInfo() const { return info_; }
+
+  bool operator==(const VMInfo &other) const {
+    return name_ == other.name_ && info_.state == other.info_.state &&
+           info_.maxMem == other.info_.maxMem && info_.memory == other.info_.memory &&
+           info_.cpuTime == other.info_.cpuTime;
+  }
+
+  std::string string() const {
+    return fmt::format("Name: {}, State: {}, MaxMem: {}, Memory: {}, CPUTime: {}", name_,
+                       LIBVIRT_STATE_MAP[info_.state], info_.maxMem, info_.memory, info_.cpuTime);
+  }
+
+ private:
+  std::string name_;
+  virDomainInfo info_;
+};
+
 class Virtualization : public ALabel {
  public:
   Virtualization(const std::string &, const Json::Value &);
@@ -22,18 +47,18 @@ class Virtualization : public ALabel {
  private:
   util::SleeperThread thread_;
 
-  // URI according to RFC 2396 to libvirt to connect to
+  // URI according to RFC 2396 to libvirt to connect to.
   std::string virt_uri_;
 
-  // Scaling factor for all eligible values of size
+  // Scaling factor for all eligible values of size.
   std::string unit_size_;
   int precision_size_;
 
-  // Scaling factor for all eligible values of time
+  // Scaling factor for all eligible values of time.
   std::string unit_time_;
   int precision_time_;
 
-  // Virtualization domains (typically VMs) to monitor
+  // Virtualization domains (typically VMs) to monitor.
   // If empty, all domains will be monitored.
   std::vector<std::string> domains_;
 
@@ -41,12 +66,18 @@ class Virtualization : public ALabel {
   // If the domain does not exist, throw an error.
   void getDomainInfo(const std::string domain, virDomainInfoPtr info);
 
+  // Get the domain info for all domains
+  std::vector<VMInfo> getAllDomains();
+
   // For a list of domain infos, calculate the total cpu and memory and the max
   // they can consume.
-  virDomainInfo getTotalDomainStats(std::vector<virDomainInfo> &domainInfos);
+  virDomainInfo getTotalDomainStats(std::vector<VMInfo> &VMInfos);
 
-  // Convert a factor of time or size for scaling
+  // Convert a factor of time or size for scaling.
   float convertFactor(std::string divisor);
+
+  // Round a float to a given precision.
+  float roundToPrecision(float num, int precision);
 };
 
 }  // namespace waybar::modules
--- src/modules/virtualization.cpp
+++ src/modules/virtualization.cpp
@@ -1,6 +1,9 @@
 #include "modules/virtualization.hpp"
 
+#include <spdlog/spdlog.h>
+
 using namespace waybar::util;
+using waybar::modules::VMInfo;
 
 waybar::modules::Virtualization::Virtualization(const std::string& id, const Json::Value& config)
     : ALabel(config, "libvirt", id, "{}%", 30) {
@@ -22,12 +25,12 @@ waybar::modules::Virtualization::Virtualization(const std::string& id, const Jso
     unit_time_ = config["unit_time"].asString();
   }
 
-  precision_size_ = 100;
+  precision_size_ = 2;
   if (config["precision_size"].isInt()) {
     precision_size_ = config["precision_size"].asInt();
   }
 
-  precision_time_ = 100;
+  precision_time_ = 2;
   if (config["precision_time"].isInt()) {
     precision_time_ = config["precision_time"].asInt();
   }
@@ -45,108 +48,158 @@ void waybar::modules::Virtualization::getDomainInfo(const std::string name, virD
   virConnectPtr con = NULL;
   virDomainPtr dom = NULL;
   int ret = EXIT_FAILURE;
+  std::string err = "";
 
   con = virConnectOpenReadOnly(virt_uri_.c_str());
   if (con == NULL) {
-    throw std::runtime_error("Unable to connect to hypervisor: " + virt_uri_);
+    err = "Unable to connect to hypervisor: " + virt_uri_;
     goto error;
   }
 
   /* Find the domain of the given name */
   dom = virDomainLookupByName(con, name.c_str());
   if (dom == NULL) {
-    throw std::runtime_error("Failed to find Domain " + name);
+    err = "Failed to find Domain " + name;
     goto error;
   }
 
   /* Get the information */
   ret = virDomainGetInfo(dom, info);
   if (ret < 0) {
-    throw std::runtime_error("Failed to get information for Domain " + name);
+    err = "Failed to get information for Domain " + name;
     goto error;
   }
 
 error:
   if (dom != NULL) virDomainFree(dom);
   if (con != NULL) virConnectClose(con);
+
+  throw std::runtime_error(err);
 }
 
-virDomainInfo waybar::modules::Virtualization::getTotalDomainStats(
-    std::vector<virDomainInfo>& domainInfos) {
-  virDomainInfo totalInfo = {0};
-
-  for (const auto& info : domainInfos) {
-    totalInfo.memory += info.memory;
-    totalInfo.maxMem += info.maxMem;
-    totalInfo.nrVirtCpu += info.nrVirtCpu;
-    totalInfo.cpuTime += info.cpuTime;
-  }
-
-  return totalInfo;
-}
-
-auto waybar::modules::Virtualization::update() -> void {
+std::vector<VMInfo> waybar::modules::Virtualization::getAllDomains() {
   virConnectPtr con = NULL;
-  virDomainPtr dom = NULL;
-  std::vector<virDomainInfo> domainInfos = {};
+  virDomainPtr domain;
+  std::vector<VMInfo> VMInfos = {};
+  int numDomains = 0, numActiveDomains = 0, numInactiveDomains = 0;
 
   con = virConnectOpenReadOnly(virt_uri_.c_str());
   if (con == NULL) {
     throw std::runtime_error("Unable to connect to hypervisor: " + virt_uri_);
-    // goto error;
   }
 
+  numActiveDomains = virConnectNumOfDomains(con);
+  numInactiveDomains = virConnectNumOfDefinedDomains(con);
+  numDomains = numActiveDomains + numInactiveDomains;
+
+  if (numDomains == 0) {
+    throw std::runtime_error("No domains found");
+  }
+
+  char** inactiveDomains = new char*[numInactiveDomains];
+  int activeDomains[numDomains] = {0};
+
+  numActiveDomains = virConnectListDomains(con, activeDomains, numActiveDomains);
+  numInactiveDomains =
+      virConnectListDefinedDomains(con, (char**)inactiveDomains, numInactiveDomains);
+
+  // active domains
+  virDomainInfo info;
+  for (int i = 0; i < numActiveDomains; i++) {
+    domain = virDomainLookupByID(con, activeDomains[i]);
+    const std::string name = std::string(virDomainGetName(domain));
+    virDomainGetInfo(domain, &info);
+    VMInfos.push_back(VMInfo(name, info));
+  }
+
+  // inactive domains
+  for (int i = 0; i < numInactiveDomains; i++) {
+    domain = virDomainLookupByName(con, inactiveDomains[i]);
+    const std::string name = std::string(virDomainGetName(domain));
+    virDomainGetInfo(domain, &info);
+    VMInfos.push_back(VMInfo(name, info));
+  }
+
+  return VMInfos;
+}
+
+virDomainInfo waybar::modules::Virtualization::getTotalDomainStats(std::vector<VMInfo>& VMInfos) {
+  virDomainInfo totalInfo = {0};
+
+  for (const auto& info : VMInfos) {
+    if (info.getInfo().state == VIR_DOMAIN_RUNNING) {
+      totalInfo.memory += info.getInfo().memory;
+      totalInfo.maxMem += info.getInfo().maxMem;
+      totalInfo.nrVirtCpu += info.getInfo().nrVirtCpu;
+      totalInfo.cpuTime += info.getInfo().cpuTime;
+    }
+  }
+  return totalInfo;
+}
+
+auto waybar::modules::Virtualization::update() -> void {
+  std::vector<waybar::modules::VMInfo> domains = getAllDomains();
+
+  // If there are no user specified domains, we will show all domains
   if (domains_.empty()) {
+    spdlog::info("libvirt: no domains supplied, showing all");
     // Empty domains means all
-    const int n = virConnectNumOfDomains(con);
-    int domains[n] = {0};
-    virConnectListDomains(con, domains, n);
-
-    auto activeDomains = malloc(sizeof(virDomainPtr) * n);
-
-    virDomainInfo info;
-    for (int i = 0; i < n; i++) {
-      const auto vir = virDomainLookupByID(con, domains[i]);
-      // TODO: check errors
-      virDomainGetInfo(vir, &info);
-      domainInfos.push_back(info);
+    domains_ = std::vector<std::string>{};
+    for (const auto& domain : domains) {
+      spdlog::info(fmt::format("libvirt: found domain {}", domain.string()));
     }
 
   } else {
-    virDomainInfo info;
-    for (const auto& domain : domains_) {
-      getDomainInfo(domain, &info);
-      domainInfos.push_back(info);
+    spdlog::info("libvirt: using specified domains");
+    for (const auto& d : domains) {
+      if (std::find(domains_.begin(), domains_.end(), d.getName()) == domains_.end()) {
+        domains.erase(std::find(domains.begin(), domains.end(), d));
+      }
     }
   }
 
-  const virDomainInfo stats = getTotalDomainStats(domainInfos);
   auto format = format_;
-
   if (format_.empty()) {
     event_box_.hide();
   } else {
     event_box_.show();
-    // if (tooltipEnabled()) {
-    //   std::string tooltip_format = "{mem_cur} used out of {mem_max}({domains})\n";
-    //   if (config_["tooltip-format"].isString()) {
-    //     tooltip_format = config_["tooltip-format"].asString();
-    //   }
 
-    //   label_.set_tooltip_text(
-    //       fmt::format(fmt::runtime(tooltip_format),
-    //                   fmt::arg("mem_max", (stats.maxMem / 1000) * convertFactor(unit_size_)),
-    //                   fmt::arg("mem_cur", (stats.memory / 1000) * convertFactor(unit_size_)),
-    //                   fmt::arg("cpu_time", (stats.cpuTime * 1000000) *
-    //                   convertFactor(unit_time_)), fmt::arg("domains", domainInfos.size())));
-    // } else {
-    label_.set_markup(
-        fmt::format(fmt::runtime(format),
-                    fmt::arg("mem_max", (stats.maxMem / 1000) / convertFactor(unit_size_)),
-                    fmt::arg("mem_cur", (stats.memory / 1000) / convertFactor(unit_size_)),
-                    fmt::arg("cpu_time", (stats.cpuTime * 1000000) / convertFactor(unit_time_)),
-                    fmt::arg("domains", domainInfos.size())));
-    // }
+    const auto stats = getTotalDomainStats(domains);
+
+    unsigned int DOMAIN_MAP[8] = {};
+    for (const auto& domain : domains) {
+      DOMAIN_MAP[domain.getInfo().state]++;
+    }
+
+    const auto mem_max =
+        roundToPrecision((stats.maxMem * 1000) / convertFactor(unit_size_), precision_size_);
+
+    const auto mem_cur =
+        roundToPrecision((stats.memory * 1000) / convertFactor(unit_size_), precision_size_);
+
+    const auto cpu_time = (stats.cpuTime * 1000000) / convertFactor(unit_time_);
+
+    if (tooltipEnabled()) {
+      std::string tooltip_format = "{mem_cur} used out of {mem_max} ({domains})\nAll Domains:\n";
+      for (const auto& v : domains) {
+        tooltip_format += fmt::format("  - {}\n", v.string());
+      }
+      if (config_["tooltip-format"].isString()) {
+        tooltip_format = config_["tooltip-format"].asString();
+      }
+
+      label_.set_tooltip_text(
+          fmt::format(fmt::runtime(tooltip_format), fmt::arg("mem_max", mem_max),
+                      fmt::arg("mem_cur", mem_cur), fmt::arg("domains", domains.size())));
+    }
+
+    label_.set_markup(fmt::format(
+        fmt::runtime(format), fmt::arg("mem_max", mem_max), fmt::arg("mem_cur", mem_cur),
+        fmt::arg("cpu_time", cpu_time), fmt::arg("domains_running", DOMAIN_MAP[1]),
+        fmt::arg("domains_blocked", DOMAIN_MAP[2]), fmt::arg("domains_paused", DOMAIN_MAP[3]),
+        fmt::arg("domains_shutdown", DOMAIN_MAP[4]), fmt::arg("domains_shutoff", DOMAIN_MAP[5]),
+        fmt::arg("domains_crashed", DOMAIN_MAP[6]), fmt::arg("domains_suspended", DOMAIN_MAP[7]),
+        fmt::arg("domains", domains.size())));
   }
 
   ALabel::update();
@@ -183,4 +236,9 @@ float waybar::modules::Virtualization::convertFactor(std::string divisor) {
   } else {
     throw std::runtime_error("invalid scaling unit: " + divisor);
   }
+}
+
+float waybar::modules::Virtualization::roundToPrecision(float num, int precision) {
+  float factor = pow(10, precision);
+  return round(num * factor) / factor;
 }
