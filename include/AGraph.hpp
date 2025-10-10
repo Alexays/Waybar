4efae35a29f1969dcf4207d44b2a7e914b18357a
Ricardo Markiewicz <ricardo.markiewicz@gmail.com>
2025-10-11 01:52:10 +0200

    feat: add gauge and stacked bar graph type

--- include/AGraph.hpp
+++ include/AGraph.hpp
@@ -11,11 +11,12 @@
 
 namespace waybar {
 
+enum class GraphType { LINE, BAR, GAUGE };
+
 class AGraph : public AModule {
  public:
-  AGraph(const Json::Value &, const std::string &, const std::string &,
-         uint16_t interval = 0, bool enable_click = false,
-         bool enable_scroll = false);
+  AGraph(const Json::Value &, const std::string &, const std::string &, uint16_t interval = 0,
+         bool enable_click = false, bool enable_scroll = false);
   virtual ~AGraph() = default;
   auto update() -> void override;
 
@@ -24,6 +25,7 @@ class AGraph : public AModule {
   std::deque<int> values_;
   uint16_t datapoints_ = 20;
   uint16_t y_offset_ = 0;
+  GraphType graph_type_ = GraphType::LINE;
 
   void addValue(const int n);
 
@@ -37,14 +39,21 @@ class AGraph : public AModule {
 
  private:
   void drawFilledArea(const Cairo::RefPtr<Cairo::Context> &cr,
-                      const std::vector<std::pair<double, double>> &points,
-                      double height, const Gdk::RGBA &bg_color);
+                      const std::vector<std::pair<double, double>> &points, double height,
+                      const Gdk::RGBA &bg_color);
 
   void drawLine(const Cairo::RefPtr<Cairo::Context> &cr,
                 const std::vector<std::pair<double, double>> &points, const Gdk::RGBA &fg_color);
 
   void drawPath(const Cairo::RefPtr<Cairo::Context> &cr,
                 const std::vector<std::pair<double, double>> &points);
+
+  void drawBars(const Cairo::RefPtr<Cairo::Context> &cr,
+                double width, double height, int current_value,
+                const Gdk::RGBA &fg_color);
+
+  void drawGauge(const Cairo::RefPtr<Cairo::Context> &cr, double width, double height,
+                 int current_value, const Gdk::RGBA &fg_color);
 };
 
 }  // namespace waybar
