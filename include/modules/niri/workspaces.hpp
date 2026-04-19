62db4718d076af6ab89c74629a81b63aab08ad1c
sewaustav <sewabobrov04@gmail.com>
2026-04-19 22:33:06 +0200

    feat(niri): add workspace-taskbar support

--- include/modules/niri/workspaces.hpp
+++ include/modules/niri/workspaces.hpp
@@ -1,30 +1,40 @@
 #pragma once
 
-#include <gtkmm/button.h>
+#include <gtkmm/box.h>
 #include <json/value.h>
+
+#include <memory>
+#include <vector>
 
 #include "AModule.hpp"
 #include "bar.hpp"
 #include "modules/niri/backend.hpp"
+#include "modules/niri/workspace.hpp"
 
 namespace waybar::modules::niri {
 
 class Workspaces : public AModule, public EventHandler {
  public:
-  Workspaces(const std::string&, const Bar&, const Json::Value&);
+  Workspaces(const std::string& id, const Bar& bar, const Json::Value& config);
   ~Workspaces() override;
+
   void update() override;
+
+  const Json::Value& config() const { return config_; }
+  const Bar& bar() const { return bar_; }
+
+  std::string getIcon(const std::string& value, const Json::Value& ws) const;
 
  private:
   void onEvent(const Json::Value& ev) override;
   void doUpdate();
-  Gtk::Button& addButton(const Json::Value& ws);
-  std::string getIcon(const std::string& value, const Json::Value& ws);
+
+  void createWorkspace(const Json::Value& workspace_data);
 
   const Bar& bar_;
   Gtk::Box box_;
-  // Map from niri workspace id to button.
-  std::unordered_map<uint64_t, Gtk::Button> buttons_;
+
+  std::vector<std::unique_ptr<Workspace>> workspaces_;
 };
 
 }  // namespace waybar::modules::niri
