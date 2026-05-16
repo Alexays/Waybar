546b53d6759c2b7cc54051889d99fccaa6c57295
Desuwa <tszymanski@google.com>
2026-05-16 15:41:09 -0700

    mpd: Implement idle events for Playing state

--- include/modules/mpd/state.hpp
+++ include/modules/mpd/state.hpp
@@ -82,6 +82,7 @@ class Idle : public State {
 class Playing : public State {
   Context* const ctx_;
   sigc::connection timer_connection_;
+  sigc::connection idle_connection_;
 
  public:
   Playing(Context* const ctx) : ctx_{ctx} {}
@@ -98,7 +99,10 @@ class Playing : public State {
   Playing(Playing const&) = delete;
   Playing& operator=(Playing const&) = delete;
 
+  void timer() noexcept;
+  void idle() noexcept;
   bool on_timer();
+  bool on_io(Glib::IOCondition const&);
 };
 
 class Paused : public State {
