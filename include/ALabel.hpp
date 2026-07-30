b45c42445b479f4f806f7ed6d44110b73751eb85
Rio Sanjaya <osianjaya@gmail.com>
2026-07-30 22:47:57 +0700

    Mark ALabel setTooltipMarkup and setLabelMarkup as virtual so  AIconLabel can implement their own method

--- include/ALabel.hpp
+++ include/ALabel.hpp
@@ -94,11 +94,6 @@ class ALabel : public AModule {
   static void handleGtkMenuEvent(GtkMenuItem* menuitem, gpointer data);
 
  private:
-  // Raw UTF-8 bytes, not Glib::ustring: ustring::operator== collates with
-  // g_utf8_collate(), which gives private-use codepoints (nerd-font icons)
-  // no collation weight, so two different icons compare equal.
-  std::optional<std::string> last_label_markup_;
-  std::optional<std::string> last_tooltip_markup_;
   Glib::RefPtr<Gtk::Tooltip> active_tooltip_;
 };
 
