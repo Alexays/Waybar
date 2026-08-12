Alexays/Waybar

commit eeb7bc702e8e20ec097e30f4fe0703b153b7e9dd

Added GeoClue2 privacy item
Rebased onto current master and fixed the privacy module build:
- privacy_item.hpp no longer includes privacy.hpp (it does not use the
  Privacy class); this broke the circular include that left PrivacyItem
  undeclared when privacy.hpp was reached first from privacy_item.cpp.
- privacy_item.cpp now includes gtkmm/label.h explicitly, since Gtk::Label
  was previously only pulled in transitively via privacy.hpp.

include/modules/privacy/privacy.hpp
@@ -1,13 +1,16 @@
 #pragma once
 
+#include <atomic>
 #include <string>
 
 #include "gtkmm/box.h"
 #include "modules/privacy/privacy_item.hpp"
+#include "util/geoclue_backend.hpp"
 #include "util/pipewire/pipewire_backend.hpp"
 #include "util/pipewire/privacy_node_info.hpp"
 
-using waybar::util::PipewireBackend::PrivacyNodeInfo;
+using waybar::util::PipewireBackend::PrivacyNodeType;
+using waybar::util::PipewireBackend::PWPrivacyNodeInfo;
 
 namespace waybar::modules::privacy {
 
@@ -16,15 +19,15 @@ class Privacy : public AModule {
   Privacy(const std::string&, const Json::Value&, Gtk::Orientation, const std::string& pos);
   auto update() -> void override;
 
-  void onPrivacyNodesChanged();
-
  private:
-  std::list<PrivacyNodeInfo*> nodes_screenshare;  // Screen is being shared
-  std::list<PrivacyNodeInfo*> nodes_audio_in;     // Application is using the microphone
-  std::list<PrivacyNodeInfo*> nodes_audio_out;    // Application is outputting audio
+  std::list<PWPrivacyNodeInfo*> nodes_screenshare;  // Screen is being shared
+  std::list<PWPrivacyNodeInfo*> nodes_audio_in;     // Application is using the microphone
+  std::list<PWPrivacyNodeInfo*> nodes_audio_out;    // Application is outputting audio
+  std::atomic<bool> location_in_use;                // GeoClue is being used
 
   std::mutex mutex_;
   sigc::connection visibility_conn;
+  sigc::connection geoclue_timeout_conn;
 
   // Config
   Gtk::Box box_;
@@ -35,7 +38,12 @@ class Privacy : public AModule {
   std::set<std::pair<PrivacyNodeType, std::string>> ignore;
   bool ignore_monitor = true;
 
-  std::shared_ptr<util::PipewireBackend::PipewireBackend> backend = nullptr;
+  std::shared_ptr<util::PipewireBackend::PipewireBackend> pw_backend = nullptr;
+  std::shared_ptr<util::GeoClueBackend::GeoClueBackend> geoclue_backend = nullptr;
+
+  void onPWPrivacyNodesChanged();
+  bool locationTimeout(bool in_use);
+  void onGeoCluePrivacyNodesChanged();
 };
 
 }  // namespace waybar::modules::privacy

src/modules/privacy/privacy.cpp
@@ -10,10 +10,11 @@
 
 namespace waybar::modules::privacy {
 
-using util::PipewireBackend::PRIVACY_NODE_TYPE_AUDIO_INPUT;
-using util::PipewireBackend::PRIVACY_NODE_TYPE_AUDIO_OUTPUT;
-using util::PipewireBackend::PRIVACY_NODE_TYPE_NONE;
-using util::PipewireBackend::PRIVACY_NODE_TYPE_VIDEO_INPUT;
+using waybar::util::PipewireBackend::PRIVACY_NODE_TYPE_AUDIO_INPUT;
+using waybar::util::PipewireBackend::PRIVACY_NODE_TYPE_AUDIO_OUTPUT;
+using waybar::util::PipewireBackend::PRIVACY_NODE_TYPE_LOCATION;
+using waybar::util::PipewireBackend::PRIVACY_NODE_TYPE_NONE;
+using waybar::util::PipewireBackend::PRIVACY_NODE_TYPE_VIDEO_INPUT;
 
 Privacy::Privacy(const std::string& id, const Json::Value& config, Gtk::Orientation orientation,
                  const std::string& pos)
@@ -21,7 +22,9 @@ Privacy::Privacy(const std::string& id, const Json::Value& config, Gtk::Orientat
       nodes_screenshare(),
       nodes_audio_in(),
       nodes_audio_out(),
+      location_in_use(false),
       visibility_conn(),
+      geoclue_timeout_conn(),
       box_(orientation, 0) {
   box_.set_name(name_);
 
@@ -45,20 +48,21 @@ Privacy::Privacy(const std::string& id, const Json::Value& config, Gtk::Orientat
 
   // Initialize each privacy module
   Json::Value modules = config_["modules"];
-  // Add Screenshare and Mic usage as default modules if none are specified
+  // Add Screenshare, Mic, and Location usage as default modules if none are specified
   if (!modules.isArray() || modules.empty()) {
     modules = Json::Value(Json::arrayValue);
-    for (const auto& type : {"screenshare", "audio-in"}) {
+    for (const auto& type : {"screenshare", "audio-in", "location"}) {
       Json::Value obj = Json::Value(Json::objectValue);
       obj["type"] = type;
       modules.append(obj);
     }
   }
 
-  std::map<std::string, std::tuple<decltype(&nodes_audio_in), PrivacyNodeType> > typeMap = {
+  std::map<std::string, std::tuple<std::list<PWPrivacyNodeInfo*>*, PrivacyNodeType>> typeMap = {
       {"screenshare", {&nodes_screenshare, PRIVACY_NODE_TYPE_VIDEO_INPUT}},
       {"audio-in", {&nodes_audio_in, PRIVACY_NODE_TYPE_AUDIO_INPUT}},
       {"audio-out", {&nodes_audio_out, PRIVACY_NODE_TYPE_AUDIO_OUTPUT}},
+      {"location", {nullptr, PRIVACY_NODE_TYPE_LOCATION}},
   };
 
   for (const auto& module : modules) {
@@ -68,8 +72,21 @@ Privacy::Privacy(const std::string& id, const Json::Value& config, Gtk::Orientat
     auto iter = typeMap.find(type);
     if (iter != typeMap.end()) {
       auto& [nodePtr, nodeType] = iter->second;
-      auto* item = Gtk::make_managed<PrivacyItem>(module, nodeType, nodePtr, orientation, pos,
+      PrivacyItem* item = nullptr;
+      switch (nodeType) {
+        case util::PipewireBackend::PRIVACY_NODE_TYPE_VIDEO_INPUT:
+        case util::PipewireBackend::PRIVACY_NODE_TYPE_AUDIO_INPUT:
+        case util::PipewireBackend::PRIVACY_NODE_TYPE_AUDIO_OUTPUT:
+          item = Gtk::make_managed<PWPrivacyItem>(module, nodeType, nodePtr, orientation, pos,
                                                   iconSize, transition_duration);
+          break;
+        case util::PipewireBackend::PRIVACY_NODE_TYPE_LOCATION:
+          item = Gtk::make_managed<GeoCluePrivacyItem>(module, orientation, pos, iconSize,
+                                                       transition_duration);
+          break;
+        case util::PipewireBackend::PRIVACY_NODE_TYPE_NONE:
+          continue;
+      }
       box_.add(*item);
       modules_.push_back(item);
     }
@@ -93,20 +110,24 @@ Privacy::Privacy(const std::string& id, const Json::Value& config, Gtk::Orientat
     ignore_monitor = config_["ignore-monitor"].asBool();
   }
 
-  backend = util::PipewireBackend::PipewireBackend::getInstance();
-  backend->privacy_nodes_changed_signal_event.connect(
-      sigc::mem_fun(*this, &Privacy::onPrivacyNodesChanged));
+  pw_backend = util::PipewireBackend::PipewireBackend::getInstance();
+  pw_backend->privacy_nodes_changed_signal_event.connect(
+      sigc::mem_fun(*this, &Privacy::onPWPrivacyNodesChanged));
+
+  geoclue_backend = util::GeoClueBackend::GeoClueBackend::getInstance();
+  geoclue_backend->in_use_changed_signal_event.connect(
+      sigc::mem_fun(*this, &Privacy::onGeoCluePrivacyNodesChanged));
 
   dp.emit();
 }
 
-void Privacy::onPrivacyNodesChanged() {
+void Privacy::onPWPrivacyNodesChanged() {
   mutex_.lock();
   nodes_audio_out.clear();
   nodes_audio_in.clear();
   nodes_screenshare.clear();
 
-  for (auto& node : backend->privacy_nodes) {
+  for (auto& node : pw_backend->privacy_nodes) {
     if (ignore_monitor && node.second->is_monitor) continue;
 
     auto iter = ignore.find(std::pair(node.second->type, node.second->node_name));
@@ -124,7 +145,7 @@ void Privacy::onPrivacyNodesChanged() {
           case PRIVACY_NODE_TYPE_AUDIO_OUTPUT:
             nodes_audio_out.push_back(node.second);
             break;
-          case PRIVACY_NODE_TYPE_NONE:
+          default:
             continue;
         }
         break;
@@ -137,36 +158,63 @@ void Privacy::onPrivacyNodesChanged() {
   dp.emit();
 }
 
+bool Privacy::locationTimeout(bool in_use) {
+  location_in_use.store(geoclue_backend->location_in_use);
+  dp.emit();
+
+  return false;
+}
+void Privacy::onGeoCluePrivacyNodesChanged() {
+  geoclue_timeout_conn.disconnect();
+
+  const bool in_use = geoclue_backend->location_in_use;
+  if (in_use) {
+    locationTimeout(in_use);
+  } else {
+    // The GeoClue service is really fast, so wait before hiding, so that the
+    // widget is at least visible for 3 seconds.
+    geoclue_timeout_conn = Glib::signal_timeout().connect_seconds(
+        sigc::bind(sigc::mem_fun(*this, &Privacy::locationTimeout), in_use), 3);
+  }
+}
+
 auto Privacy::update() -> void {
   // set in modules or not
   bool setScreenshare = false;
   bool setAudioIn = false;
   bool setAudioOut = false;
+  bool setLocation = false;
 
   // used or not
   bool useScreenshare = false;
   bool useAudioIn = false;
   bool useAudioOut = false;
+  bool useLocation = false;
 
   mutex_.lock();
   for (PrivacyItem* module : modules_) {
     switch (module->privacy_type) {
-      case util::PipewireBackend::PRIVACY_NODE_TYPE_VIDEO_INPUT:
+      case PRIVACY_NODE_TYPE_VIDEO_INPUT:
         setScreenshare = true;
         useScreenshare = !nodes_screenshare.empty();
         module->set_in_use(useScreenshare);
         break;
-      case util::PipewireBackend::PRIVACY_NODE_TYPE_AUDIO_INPUT:
+      case PRIVACY_NODE_TYPE_AUDIO_INPUT:
         setAudioIn = true;
         useAudioIn = !nodes_audio_in.empty();
         module->set_in_use(useAudioIn);
         break;
-      case util::PipewireBackend::PRIVACY_NODE_TYPE_AUDIO_OUTPUT:
+      case PRIVACY_NODE_TYPE_AUDIO_OUTPUT:
         setAudioOut = true;
         useAudioOut = !nodes_audio_out.empty();
         module->set_in_use(useAudioOut);
         break;
-      case util::PipewireBackend::PRIVACY_NODE_TYPE_NONE:
+      case PRIVACY_NODE_TYPE_LOCATION:
+        setLocation = true;
+        useLocation = location_in_use;
+        module->set_in_use(useLocation);
+        break;
+      case PRIVACY_NODE_TYPE_NONE:
         break;
     }
   }
@@ -174,7 +222,7 @@ auto Privacy::update() -> void {
 
   // Hide the whole widget if none are in use
   bool isVisible = (setScreenshare && useScreenshare) || (setAudioIn && useAudioIn) ||
-                   (setAudioOut && useAudioOut);
+                   (setAudioOut && useAudioOut) || (setLocation && useLocation);
 
   if (isVisible != event_box_.get_visible()) {
     // Disconnect any previous connection so that it doesn't get activated in
@@ -187,12 +235,13 @@ auto Privacy::update() -> void {
       // have finished animating
       visibility_conn = Glib::signal_timeout().connect(
           sigc::track_obj(
-              [this, setScreenshare, setAudioOut, setAudioIn]() {
+              [this, setScreenshare, setAudioOut, setAudioIn, setLocation]() {
                 mutex_.lock();
                 bool visible = false;
                 visible |= setScreenshare && !nodes_screenshare.empty();
                 visible |= setAudioIn && !nodes_audio_in.empty();
                 visible |= setAudioOut && !nodes_audio_out.empty();
+                visible |= setLocation && location_in_use;
                 mutex_.unlock();
                 event_box_.set_visible(visible);
                 return false;

include/modules/privacy/privacy_item.hpp
@@ -9,27 +9,29 @@
 #include "gtkmm/revealer.h"
 #include "util/pipewire/privacy_node_info.hpp"
 
-using waybar::util::PipewireBackend::PrivacyNodeInfo;
 using waybar::util::PipewireBackend::PrivacyNodeType;
+using waybar::util::PipewireBackend::PWPrivacyNodeInfo;
 
 namespace waybar::modules::privacy {
 
 class PrivacyItem : public Gtk::Revealer {
- public:
+ protected:
   PrivacyItem(const Json::Value& config_, enum PrivacyNodeType privacy_type_,
-              std::list<PrivacyNodeInfo*>* nodes, Gtk::Orientation orientation,
-              const std::string& pos, const uint icon_size, const uint transition_duration);
+              Gtk::Orientation orientation, const std::string& pos, const uint icon_size,
+              const uint transition_duration);
+
+ public:
+  virtual void set_tooltip() = 0;
 
   enum PrivacyNodeType privacy_type;
 
   void set_in_use(bool in_use);
 
- private:
-  std::list<PrivacyNodeInfo*>* nodes;
-
-  sigc::connection signal_conn;
-
+  uint tooltipIconSize = 24;
   Gtk::Box tooltip_window;
+
+ private:
+  sigc::connection signal_conn;
 
   bool init = false;
   bool in_use = false;
@@ -37,12 +39,35 @@ class PrivacyItem : public Gtk::Revealer {
   // Config
   std::string iconName = "image-missing-symbolic";
   bool tooltip = true;
-  uint tooltipIconSize = 24;
 
   Gtk::Box box_;
   Gtk::Image icon_;
 
   void update_tooltip();
+};
+
+class GeoCluePrivacyItem : public PrivacyItem {
+ public:
+  GeoCluePrivacyItem(const Json::Value& config_, Gtk::Orientation orientation,
+                     const std::string& pos, const uint icon_size, const uint transition_duration)
+      : PrivacyItem(config_, util::PipewireBackend::PRIVACY_NODE_TYPE_LOCATION, orientation, pos,
+                    icon_size, transition_duration) {}
+
+  void set_tooltip() override;
+};
+
+class PWPrivacyItem : public PrivacyItem {
+ public:
+  PWPrivacyItem(const Json::Value& config_, enum PrivacyNodeType privacy_type_,
+                std::list<PWPrivacyNodeInfo*>* nodes_, Gtk::Orientation orientation,
+                const std::string& pos, const uint icon_size, const uint transition_duration)
+      : PrivacyItem(config_, privacy_type_, orientation, pos, icon_size, transition_duration),
+        nodes(nodes_) {}
+
+  void set_tooltip() override;
+
+ private:
+  std::list<PWPrivacyNodeInfo*>* nodes;
 };
 
 }  // namespace waybar::modules::privacy

include/util/geoclue_backend.hpp
@@ -1,0 +1,43 @@
+#pragma once
+
+#include <giomm/dbusconnection.h>
+
+#include <atomic>
+
+#include "giomm/dbusproxy.h"
+
+namespace waybar::util::GeoClueBackend {
+
+class GeoClueBackend {
+ private:
+  guint watcherID_;
+  sigc::connection signal_conn;
+  Glib::RefPtr<Gio::DBus::Proxy> proxy;
+  bool connected;
+
+  /* Hack to keep constructor inaccessible but still public.
+   * This is required to be able to use std::make_shared.
+   * It is important to keep this class only accessible via a reference-counted
+   * pointer because the destructor will manually free memory, and this could be
+   * a problem with C++20's copy and move semantics.
+   */
+  struct PrivateConstructorTag {};
+
+ public:
+  sigc::signal<void> in_use_changed_signal_event;
+
+  std::atomic<bool> location_in_use;  // GeoClue is being used
+
+  static std::shared_ptr<GeoClueBackend> getInstance();
+
+  // DBus callbacks
+  void onAppear(const Glib::RefPtr<Gio::DBus::Connection>&, const Glib::ustring&,
+                const Glib::ustring&);
+  void onVanished(const Glib::RefPtr<Gio::DBus::Connection>&, const Glib::ustring&);
+  void propertyChanged(const Gio::DBus::Proxy::MapChangedProperties& changedProperties,
+                       const std::vector<Glib::ustring>& invalidatedProperties);
+
+  GeoClueBackend(PrivateConstructorTag tag);
+  ~GeoClueBackend();
+};
+}  // namespace waybar::util::GeoClueBackend

include/util/pipewire/pipewire_backend.hpp
@@ -29,7 +29,7 @@ class PipewireBackend {
  public:
   sigc::signal<void> privacy_nodes_changed_signal_event;
 
-  std::unordered_map<uint32_t, PrivacyNodeInfo*> privacy_nodes;
+  std::unordered_map<uint32_t, PWPrivacyNodeInfo*> privacy_nodes;
   std::mutex mutex_;
 
   static std::shared_ptr<PipewireBackend> getInstance();

include/util/pipewire/privacy_node_info.hpp
@@ -12,10 +12,11 @@ enum PrivacyNodeType {
   PRIVACY_NODE_TYPE_NONE,
   PRIVACY_NODE_TYPE_VIDEO_INPUT,
   PRIVACY_NODE_TYPE_AUDIO_INPUT,
-  PRIVACY_NODE_TYPE_AUDIO_OUTPUT
+  PRIVACY_NODE_TYPE_AUDIO_OUTPUT,
+  PRIVACY_NODE_TYPE_LOCATION
 };
 
-class PrivacyNodeInfo {
+class PWPrivacyNodeInfo {
  public:
   PrivacyNodeType type = PRIVACY_NODE_TYPE_NONE;
   uint32_t id;

man/waybar-privacy.5.scd
@@ -28,7 +28,7 @@ the screen or playing audio.
 
 *modules* ++
 	typeof: array of objects ++
-	default: [{"type": "screenshare"}, {"type": "audio-in"}] ++
+	default: [{"type": "screenshare"}, {"type": "audio-in"}, {"type": "location"}] ++
 	Which privacy modules to monitor. See *MODULES CONFIGURATION* for++
 	more information.
 
@@ -52,8 +52,12 @@ the screen or playing audio.
 
 *type*: ++
 	typeof: string ++
-	values: "screenshare", "audio-in", "audio-out" ++
+	values: "screenshare", "audio-in", "audio-out", "location" ++
 	Specifies which module to use and configure.
+
+*icon-name*: ++
+	typeof: string ++
+	If set, changes the modules icon.
 
 *tooltip*: ++
 	typeof: bool ++
@@ -95,6 +99,10 @@ the screen or playing audio.
 			"type": "audio-in",
 			"tooltip": true,
 			"tooltip-icon-size": 24
+		},
+		{
+			"type": "location",
+			"icon-name": "location-services-active-symbolic"
 		}
 	],
 	"ignore-monitor": true,
@@ -118,3 +126,4 @@ the screen or playing audio.
 - *#privacy-item.screenshare*
 - *#privacy-item.audio-in*
 - *#privacy-item.audio-out*
+- *#privacy-item.location*

meson.build
@@ -426,6 +426,7 @@ if pipewire.found()
         'src/modules/privacy/privacy_item.cpp',
         'src/util/pipewire/pipewire_backend.cpp',
         'src/util/pipewire/privacy_node_info.cpp',
+        'src/util/geoclue_backend.cpp',
     )
     man_files += files('man/waybar-privacy.5.scd')
 endif

commit 38cc11c3b083c473e421ac9a6ead16e77f3469eb

privacy: disconnect pending timeouts on destruction
The privacy module registers timeout callbacks in the GLib main context.
The GeoClue callback uses sigc::mem_fun() with the Privacy object, but the
connection remains active when the module is destroyed during a bar reload.

If the timeout runs after destruction, it dereferences the freed Privacy
object and crashes Waybar:

  #0 waybar::modules::privacy::Privacy::locationTimeout(bool)
  #4 g_main_context_iteration()
  #5 g_application_run()

Disconnect the GeoClue and visibility timeout connections in the Privacy
destructor so no callback can run after the module is gone.

The unpatched binary crashed after four SIGUSR2 reloads at 1.5-second
intervals. The patched binary survived ten reloads.

Fixes: eeb7bc702e8e ("Added GeoClue2 privacy item")
Assisted-by: Claude Opus + Cursor

include/modules/privacy/privacy.hpp
@@ -17,6 +17,7 @@ namespace waybar::modules::privacy {
 class Privacy : public AModule {
  public:
   Privacy(const std::string&, const Json::Value&, Gtk::Orientation, const std::string& pos);
+  ~Privacy() override;
   auto update() -> void override;
 
  private:

src/modules/privacy/privacy.cpp
@@ -119,6 +119,15 @@ Privacy::Privacy(const std::string& id, const Json::Value& config, Gtk::Orientat
       sigc::mem_fun(*this, &Privacy::onGeoCluePrivacyNodesChanged));
 }
 
+Privacy::~Privacy() {
+  // Both timeouts are attached to the global main context, which outlives this
+  // module when the bar is reloaded. sigc::connection's destructor does not
+  // disconnect, and the members are torn down after the backends they use, so a
+  // pending timeout would dispatch onto freed memory.
+  geoclue_timeout_conn.disconnect();
+  visibility_conn.disconnect();
+}
+
 void Privacy::onPWPrivacyNodesChanged() {
   mutex_.lock();
   nodes_audio_out.clear();